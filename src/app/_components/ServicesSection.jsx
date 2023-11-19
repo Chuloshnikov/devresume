0e894cbcbe86bc6954d14563a13075ecad163ddf
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-19 13:22:06 +0200

    done

--- src/app/_components/ServicesSection.jsx
+++ src/app/_components/ServicesSection.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
-const ServicesSection = () => {
+const ServicesSection = ({ data }) => {
+  const { title, service_cards } = data;
   return (
-    <div>ServicesSection</div>
+    <section className="pb-32 pt-24">
+      <h2 className="text-4xl font-bold text-center mb-20">{title}</h2>
+      <div className="py-4 px-4 mx-auto max-w-screen-xl">
+        <div className="">
+          {service_cards.map((s) => (
+            <ServiceCard data={s} key={s.title} />
+          ))}
+        </div>
+      </div>
+  </section>
   )
 }
 
