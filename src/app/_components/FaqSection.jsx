db42176a54484e540625d0680f646bdc0d87d389
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-19 14:15:25 +0200

    done footer +

--- src/app/_components/FaqSection.jsx
+++ src/app/_components/FaqSection.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const FaqSection = () => {
+const FaqSection = ({ data }) => {
+  const { title, faq_cards } = data;
   return (
-    <div>FaqSection</div>
-  )
+    <section className="px-52 bg-zinc-950 pt-20 pb-36">
+      <h2 className="text-4xl font-bold text-center mb-20">{title}</h2>
+      <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-2">
+        {faq_cards.map((f) => (
+          <FaqCard data={f} key={f.question} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default FaqSection;
