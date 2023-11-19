85f3a1ee7cf676ea1bb85f5df949b1e88a1d172b
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-19 13:44:43 +0200

    done

---
+++ src/app/_components/Avatar.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Avatar = (props) => {
+    return (
+      <div className="flex items-center mt-3 space-x-3">
+        <div className="flex-shrink-0 overflow-hidden rounded-full w-10 h-10">
+          <Image src={props.image} width="100" height="100" alt="Avatar" />
+        </div>
+        <div>
+          <div className="text-lg font-medium">{props.name}</div>
+        </div>
+      </div>
+    );
+  };
+
+
+export default Avatar;
--- src/app/_components/ContactSection.jsx
+++ src/app/_components/ContactSection.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import Container from "./Container";
+import { EmailIcon, PhoneIcon, LocationIcon } from './ContactIcons';
 
-const ContactSection = () => {
+const ContactSection = ({ data }) => {
+  const { title, description, email, phone, location } = data;
   return (
-    <div>ContactSection</div>
-  )
+    <Container className="flex w-full flex-col my-16 items-center justify-center text-center">
+      <div className="text-xl" id="contact">
+        <h2 className="text-4xl font-bold text-center mb-8">{title}</h2>
+        <p className="pb-8">{description}</p>
+        <span className="block mb-2">
+          <EmailIcon /> {email}
+        </span>
+        <span className="block mb-2">
+          <PhoneIcon /> {phone}
+        </span>
+        <span className="block mb-2">
+          <LocationIcon /> {location}
+        </span>
+      </div>
+    </Container>
+  );
 }
+
+
 
 export default ContactSection;
