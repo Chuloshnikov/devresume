1f7490b4224eb6d6828b2aee219ac31852ca46d9
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-20 14:17:08 +0200

    done

--- src/app/page.js
+++ src/app/page.js
@@ -11,15 +11,16 @@ const getLandingPageData = async () => {
   const version = process.env.SB_DATA_VERSION;
   const token = process.env.SB_TOKEN;
   const url = `https://api.storyblok.com/v2/cdn/stories/landing-page?version=${version}&token=${token}`;
-  let req = await fetch(url, {cach: "no-store"});
+  let req = await fetch(url, {cache: 'no-store'});
 
   const storyData = await req.json();
-  const { nav_section, hero_section, services_section } = storyData.story.content;
-
+  const { nav_section, hero_section, services_section, testimonials_section, } = storyData.story.content;
+  console.log(storyData.story.content);
   return {
     nav_section: nav_section[0],
     hero_section: hero_section[0],
     services_section: services_section[0],
+    testimonials_section: testimonials_section[0],
   }
 }
 
@@ -31,8 +32,9 @@ export default async function Home() {
       <Navbar data={storyData.nav_section} />
       <HeroSection data={storyData.hero_section}/>
       <ServicesSection data={storyData.services_section}/>
+      
+      <TestimonialsSection data={storyData.testimonials_section}/>
       {/*
-      <TestimonialsSection/>
       <ContactSection/>
       <FaqSection/>
       <Footer/>
