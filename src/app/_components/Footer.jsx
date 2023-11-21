30fa52847136399ea07ce2333b8208fcf29331c1
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-21 15:45:19 +0200

    done

--- src/app/_components/Footer.jsx
+++ src/app/_components/Footer.jsx
@@ -1,8 +1,13 @@
 const Footer = ({ data }) => {
-  const { text } = data;
+  const { text, title } = data;
   return (
-    <div className="flex w-full flex-col mt-4 items-center justify-center text-center pt-10 mt-5 mb-10">
-      {text}
+    <div className="flex w-full flex-col gap-8 mt-4 items-center justify-center text-center pt-10 mt-5 mb-10">
+      <h3
+      className="text-2xl font-bold"
+      >
+        {title}
+      </h3>
+      <span>{text}</span>
     </div>
   );
 };
