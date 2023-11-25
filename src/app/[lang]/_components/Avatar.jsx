c9dd68d18c97ee17124e600a5fbf03443897e82b
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-25 18:53:38 +0200

    done
    swiper fix need

--- src/app/[lang]/_components/Avatar.jsx
+++ src/app/[lang]/_components/Avatar.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Avatar = (props) => {
     return (
       <div className="flex items-center mt-3 space-x-3">
-        <div className="flex-shrink-0 overflow-hidden rounded-full w-10 h-10">
+        <div className="flex-shrink-0 overflow-hidden rounded-full w-10 h-10 border-2 shadow border-gray-600">
           <Image src={props.image} width="100" height="100" alt="Avatar" />
         </div>
         <div>
