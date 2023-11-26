719f55b56be0a9152a78340ffd4095fd190ffd9b
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-26 19:14:38 +0200

    done

--- src/app/[lang]/_components/FaqSection.jsx
+++ src/app/[lang]/_components/FaqSection.jsx
@@ -4,7 +4,7 @@ import FaqCard from './FaqCard';
 const FaqSection = ({ data }) => {
   const { title, faq_cards } = data;
   return (
-    <section className="px-52 bg-zinc-950 pt-20 pb-36">
+    <section className="px-4 xl:px-52 bg-zinc-950 pt-20 pb-36">
       <h2 className="text-4xl font-bold text-center mb-20">{title}</h2>
         {faq_cards.map((f) => (
           <FaqCard data={f} key={f.question} />
