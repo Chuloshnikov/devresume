10526b85e84ee9186bfd33ad5e9e3f89f8ab85dc
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-26 17:49:09 +0200

    done

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -37,9 +37,9 @@ export default async function Home({ params: {lang}}) {
       <ServicesSection data={storyData.services_section}/>
       
       <TestimonialsSection data={storyData.testimonials_section}/>
-      {/*
-      <ContactSection data={storyData.contact_section}/>
       
+      <ContactSection data={storyData.contact_section}/>
+      {/*
       <FaqSection data={storyData.faq_section}/>
       
       <Footer data={storyData.footer_section}/>
