6daded96af2a6c4e65e384249e0523f435fe335c
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-12-01 17:19:59 +0200

    done

--- src/app/[lang]/_components/FaqCard.jsx
+++ src/app/[lang]/_components/FaqCard.jsx
@@ -1,7 +1,6 @@
 "use client"
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Accordion from '@mui/material/Accordion';
 
